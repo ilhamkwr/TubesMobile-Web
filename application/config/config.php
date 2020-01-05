7bc7c9050576aff2872894473adef7f1903a69d0
Bayhaqi101 <39187403+TippedLeader110@users.noreply.github.com>
2020-01-05 16:01:07 +0700

    Update base url

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/sensasiq';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/sensasiq';
 
 /*
 |--------------------------------------------------------------------------
